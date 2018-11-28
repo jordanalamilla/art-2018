a3e1750b093058a09f8467c3ccae6361b468aea6
Jordan Alamilla <jordan.alamilla@gmail.com>
2018-11-28 15:44:01 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    // CONVERT VERTICAL SCROLLING TO HORIZONTAL
+    scrollConverter.activate();
+
+    // VARIABLE TO AVOID MULTIPLE MOUSEMOVE()'S
+    let oldTimestamp = 0;
+
+    // HIDE OVERLAY AND DRAWING INFO ON PAGE LOAD
+    $( '#overlay' ).delay( 5500 ).fadeOut( 500 );
+    $( '.drawing-info' ).delay( 5500 ).fadeOut( 500 );
+
+    // WHEN THE MOUSE IS MOVED, TEMPORARILY SHOW OVERLAY AND DRAWING INFO
+    $( 'body' ).mousemove((_e) => {
+
+        if( ( _e.timeStamp - oldTimestamp ) > 6000 ) {
+            $( '#overlay' ).fadeIn( 500 ).delay( 5000 ).fadeOut( 500 );
+            $( '.drawing-info' ).fadeIn( 500 ).delay( 5000 ).fadeOut( 500 );
+            oldTimestamp = _e.timeStamp;
+        }
+
+    });
+});
